424dfef0a92af39fcc789b1e5047725f720ffea7
Gus <xiaoxian.duan@gmail.com>
2018-12-21 19:48:37 +0800

    LoginVC UI finished.

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Xiaoxian Duan on 12/21/18.
+//  Copyright © 2018 Xiaoxian Duan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Xiaoxian Duan on 12/21/18.
+//  Copyright © 2018 Xiaoxian Duan. All rights reserved.
+//
+
+import Foundation
+
+//  Segues
+
+let TO_LOGIN = "toLogin"
