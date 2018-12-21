0e61e6ab8b17fa135c813f75b050fd063aa8e658
Gus <xiaoxian.duan@gmail.com>
2018-12-21 12:13:06 +0800

    rearViewRevealWidth changed.

---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Xiaoxian Duan on 12/21/18.
+//  Copyright © 2018 Xiaoxian Duan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+
+}
